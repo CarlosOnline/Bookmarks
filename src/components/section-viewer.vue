59e048108b938916c39cc967682a989e1abd2cf9
Carlos Gomes <cgomes@iinet.com>
2024-04-02 19:26:20 -0700

    Revert to classic heading

--- src/components/section-viewer.vue
+++ src/components/section-viewer.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import { Section } from '@/services/bookmarks';
-import { getBookmarkBackgroundColor, getBookmarkColor } from '@/services/colors';
 
-const props = defineProps<{
+defineProps<{
     section: Section,
 }>()
 
@@ -11,25 +9,11 @@ const emit = defineEmits<{
     click: [section: Section],
 }>();
 
-function calculateBackgroundColor(section: Section) {
-    const color = getBookmarkColor(section, null);
-    if (color == "white") {
-        return getBookmarkBackgroundColor(section, null);
-    }
-
-    return color;
-}
-
-const backgroundColor = computed(() => calculateBackgroundColor(props.section));
-const color = computed(() => getBookmarkBackgroundColor(props.section, null));
 </script>
 
 <template>
     <div class="section-viewer">
-        <h3 class="btn btn-outline-info" :style="{
-            color: color + ' !important',
-            borderColor: backgroundColor + ' !important'
-        }" @click="emit('click', section)">{{ section.name }}</h3>
+        <h3 @click="emit('click', section)">{{ section.name }}</h3>
     </div>
 </template>
 
@@ -39,7 +23,6 @@ const color = computed(() => getBookmarkBackgroundColor(props.section, null));
     border-radius: 0.5rem;
 
     h3 {
-        font-size: var(--section-font-size) !important;
         min-width: 8rem;
     }
 }
